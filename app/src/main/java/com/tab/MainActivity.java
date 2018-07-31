3593ae798895191ef82a4d30caca7de26e8f8fce
liangchaojie <liangchaojie1@126.com>
2018-07-31 09:18:06 +0800

    更改包名为com.tab

---
+++ app/src/main/java/com/tab/MainActivity.java
@@ -0,0 +1,56 @@
+package com.tab;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+
+import com.tab.views.MyTabIndicator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private MyTabIndicator tabIndicator;
+    private ViewPager viewPager;
+    private List<Fragment> fragmentList;
+    private List<String> tab_list;
+    private MyAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabIndicator = findViewById(R.id.tabIndicator);
+        viewPager = findViewById(R.id.viewpage);
+
+        initData();
+
+        adapter = new MyAdapter(getSupportFragmentManager(),fragmentList,tab_list);
+        viewPager.setAdapter(adapter);
+        tabIndicator.setupWithViewPager(viewPager);
+
+    }
+
+    private void initData() {
+        fragmentList = new ArrayList<>();
+        fragmentList.add(new BlankFragment());
+        fragmentList.add(new BlankFragment());
+        fragmentList.add(new BlankFragment());
+        fragmentList.add(new BlankFragment());
+        fragmentList.add(new BlankFragment());
+
+
+        tab_list = new ArrayList<>();
+        tab_list.add("科技");
+        tab_list.add("创业");
+        tab_list.add("历史");
+        tab_list.add("家庭");
+        tab_list.add("伦理");
+    }
+
+}
---
+++ app/src/main/java/com/tab/views/MyViewPagerHelper.java
@@ -0,0 +1,26 @@
+package com.tab.views;
+
+import android.support.v4.view.ViewPager;
+
+public class MyViewPagerHelper {
+    public MyViewPagerHelper() {
+    }
+
+    public static void bind(final MyTabIndicator tabIndicator, ViewPager pager) {
+        if (tabIndicator != null && pager != null) {
+            pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+                public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                    tabIndicator.onPageScrolled(position, positionOffset, positionOffsetPixels);
+                }
+
+                public void onPageSelected(int position) {
+                    tabIndicator.onPageSelected(position);
+                }
+
+                public void onPageScrollStateChanged(int state) {
+                    tabIndicator.onPageScrollStateChanged(state);
+                }
+            });
+        }
+    }
+}
